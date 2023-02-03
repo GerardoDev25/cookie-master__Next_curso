188ab43ae1e7c87b8177ddc2a13397e02060b27a
Gerardo Miranda <gerardodev25@gmail.com>
2023-02-03 09:14:59 -0400

    6. Página para cambiar el tema

--- src/pages/theme-change.tsx
+++ src/pages/theme-change.tsx
@@ -1,11 +1,38 @@
-import { Layout } from 'components/layout';
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { NextPage } from 'next';
+import {
+  Card,
+  CardContent,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from '@mui/material';
+
+import { Layout } from 'components/layout';
 
 const ThemeChangePage: NextPage = () => {
+  const [currenttheme, setCurrenttheme] = useState('ligth');
+
+  const onThemeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCurrenttheme(e.target.value);
+  };
+
   return (
     <Layout>
-      <h1>ThemeChangePage</h1>
+      <Card>
+        <CardContent>
+          <FormControl>
+            <FormLabel>Theme</FormLabel>
+            <RadioGroup value={currenttheme} onChange={onThemeChange}>
+              <FormControlLabel value={'ligth'} control={<Radio />} label='ligth' />
+              <FormControlLabel value={'dark'} control={<Radio />} label='Dark' />
+              <FormControlLabel value={'custom'} control={<Radio />} label='Custom' />
+            </RadioGroup>
+          </FormControl>
+        </CardContent>
+      </Card>
     </Layout>
   );
 };
